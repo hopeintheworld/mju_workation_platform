8d11a12b3bb41b1632da245b4a2cee4bf64e5bed
hopeintheworld <jyissac9@gmail.com>
2023-10-23 17:05:50 +0900

    20231023 Testing Connection with MySQL

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// MySQL Config.
+module.exports = {
+    host: 'localhost',
+    user: 'root', //' < MySQL username >
+    password: '7410sjy@', // < MySQL password >
+    database: 'workation_db' // < MySQL Database name >
+};
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+// NodeJS Initial Setting.
 const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// MySQL Initial Setting.
+const mysql = require('mysql');
+const dbconfig = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
+
+connection.connect();
+
+// NodeJS Start.
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+});
+
+app.get('/users', (req, res) => {
+    connection.query('SELECT * FROM Users', (error, rows, fields) => {
+        if (error) throw error;
+        console.log('User info is: ', rows);
+        res.send(rows);
+    });
+
+    connection.end();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
